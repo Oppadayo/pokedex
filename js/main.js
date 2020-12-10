45ff25e94f6045eb9a400f8483cb0e0bc971071a
Paula Correa <47601436+Oppadayo@users.noreply.github.com>
2020-12-10 18:39:54 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 const main = document.querySelector('.cards')
 
-const fetchPokemons = async () => {  
 
-  for(let i = 1; i <= 898; i++){
-    await getPokemons(i)
-  }
+const fetchPokemons = async () =>{
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898`
+  const res = await fetch(pokemonUrl)
+  const pokemon = await res.json()  
+  
+  getPokemons(pokemon.results)
 }
 
-const getPokemons = async id =>{
-  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}`
-
-  const res = await fetch(pokemonUrl)
-  const pokemon = await res.json()
-
-  createPokemonCard(pokemon)
+const getPokemons = async poke =>{  
+  
+  for(let prop in poke){
+    const pokemonUrl = poke[prop].url
+    
+    const res = await fetch(pokemonUrl)
+    const pokemon = await res.json()
+    
+    createPokemonCard(pokemon)
+    
+  }   
+  
 }
 
 fetchPokemons()
@@ -45,52 +52,4 @@ function createPokemonCard(pokemon){
   main.appendChild(pokemonEl)
   
 }
-const main = document.querySelector('.cards')
-const fetchPokemons = async () => {  
 
-  for(let i = 1; i <= 898; i++){
-    await getPokemons(i)
-  }
-}
-
-const getPokemons = async id =>{
-  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}`
-
-  const res = await fetch(pokemonUrl)
-  const pokemon = await res.json()
-
-  createPokemonCard(pokemon)
-}
-
-fetchPokemons()
-
-function createPokemonCard(pokemon){
-  const pokemonEl = document.createElement('div')
-
-  const types = pokemon.types.map(el => el.type.name)
-  
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-
-  pokemonEl.classList.add('card', types[0])
-
-  const pokemonInnerHTML = `
-  
-  <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
-  <div class="pokemon">
-    <div class="info">
-      <h2 class="name">${name}</h2>
-      <div class="types">
-       <span class="type one">${types[0]}</span>         
-      <span class="type ${types[1] ? 'two' : 'hide' }">${types[1]}</span>
-      </div>   
-      </div>
-    <div class="pokemon-img">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}" class="poke-img" />
-    </div>
-  </div>
-  `
-
-  pokemonEl.innerHTML = pokemonInnerHTML
-
-  main.appendChild(pokemonEl)
-}
